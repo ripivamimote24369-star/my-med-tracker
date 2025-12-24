75a35242bad1252190c8db00f522443b67751d76
ripivamimote24369-star <ripivamimote24369@gmail.com>
2025-12-24 18:35:30 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,13 @@
-const CACHE_NAME = 'med-tracker-v1';
+// 修改 sw.js 的第一行
+const CACHE_NAME = 'med-tracker-v2'; // 只要改变这个字符串，手机就会识别到新版本
 const ASSETS = [
   'index.html',
   'manifest.json'
 ];
 
 self.addEventListener('install', (e) => {
+  // 强制跳过等待，立即激活新版本
+  self.skipWaiting();
   e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
 });
 
